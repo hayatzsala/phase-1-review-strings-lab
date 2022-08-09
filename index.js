d76702e358765bdfff863754dfbff7a3a69748ef
hayatSalawdeh <salawdeh.hayat@gmail.com>
2022-08-10 01:49:37 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+var currentUser = 'hayat';
+var welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+var excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+var shortGreeting =`Welcome, ${currentUser[0]}!`;
